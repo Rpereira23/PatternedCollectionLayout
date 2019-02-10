a97de758075a17f98ddb5b40d0177516dfa8193f
Raique <raique.pereira23@gmail.com>
2019-02-10 17:36:04 -0500

    initial commit

---
+++ PatternedCollectionLayout/PatternedCollectionLayout.h
@@ -0,0 +1,19 @@
+//
+//  PatternedCollectionLayout.h
+//  PatternedCollectionLayout
+//
+//  Created by Raique Pereira on 11/13/18.
+//  Copyright © 2018 Raique Pereira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PatternedCollectionLayout.
+FOUNDATION_EXPORT double PatternedCollectionLayoutVersionNumber;
+
+//! Project version string for PatternedCollectionLayout.
+FOUNDATION_EXPORT const unsigned char PatternedCollectionLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PatternedCollectionLayout/PublicHeader.h>
+
+
